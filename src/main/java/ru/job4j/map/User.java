b5d95ee7806608b033e06a93216300efd1397d55
Viktor <mannar@inbox.ru>
2021-12-05 03:57:28 +0500

    3. Переопределить только hashCode [#1003] 0 2

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -3,6 +3,7 @@ package ru.job4j.map;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private final String name;
@@ -12,14 +13,11 @@ public class User {
     public User(String name, int children) {
         this.name = name;
         this.children = children;
-        birthday = new GregorianCalendar();
+        birthday = new GregorianCalendar(2021, 11, 05);
     }
-
-    public static void main(String[] args) {
-        User one = new User("User", 1);
-        User two = new User("User", 1);
-        Map<User, Object> map = Map.of(one, new Object(), two, new Object());
-        map.forEach((user, object) -> System.out.println(user + " " + object));
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 
     @Override
@@ -28,6 +26,13 @@ public class User {
 +               "name='" + name + '\'' 
 +                ", children=" + children 
 +                ", birthday=" + birthday.getTime() 
-+                '}';
+        +                '}';
+    }
+
+    public static void main(String[] args) {
+        User one = new User("User", 1);
+        User two = new User("User", 1);
+        Map<User, Object> map = Map.of(one, new Object(), two, new Object());
+        map.forEach((user, object) -> System.out.println(user + " " + object));
     }
 }
