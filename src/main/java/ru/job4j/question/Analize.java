95d59d72ff8555c17b3a58b6fdb1aab80d3faa81
Viktor <mannar@inbox.ru>
2021-12-09 16:10:03 +0500

    2. Статистика по коллекции. [#45889] 0 2

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,25 @@
+package ru.job4j.question;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Analize {
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int del = 0;
+        int changed = 0;
+        int added = 0;
+        Map<Integer, String> map = current.stream()
+                .collect(Collectors.toMap(user -> user.getId(), user -> user.getName()));
+        for (User user : previous) {
+            String name = map.remove(user.getId());
+            if (name != null && !name.equals(user.getName())) {
+                changed++;
+            }
+            if (name == null) {
+                del++;
+            }
+        }
+        return new Info(map.size(), changed, del);
+    }
+}
