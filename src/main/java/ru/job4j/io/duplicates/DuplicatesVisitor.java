dc81f6a31ee5eea0bf2727e3656f8b8bb50d25b9
Viktor <mannar@inbox.ru>
2021-12-15 01:59:22 +0500

    4.2. Поиск дубликатов [#315066 #240123] 0 8

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -2,24 +2,29 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final Set<FileProperty> pathes = new HashSet<>();
+    private final Map<FileProperty, List<String>> pathes = new HashMap<>();
+
+    public void printDuplicate() {
+        pathes.values().stream()
+                .filter(l -> l.size() > 1)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
-        if (!pathes.add(new FileProperty(file.toFile().getName(), file.toFile().length()))) {
-            Files.delete(file);
-            System.out.println("Дубликат: " + file.getFileName() + " удален!");
-        }
-
+        FileProperty fileProperty = new FileProperty(file.toFile().getName(), file.toFile().length());
+        pathes.putIfAbsent(fileProperty, new ArrayList<>());
+        pathes.get(fileProperty).add(file.toFile().getAbsolutePath());
         return super.visitFile(file, attrs);
     }
 }
