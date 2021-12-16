263558690be5b4e772ad12edc8a8874e04ac1a14
Viktor <mannar@inbox.ru>
2021-12-16 20:29:26 +0500

    5.2. Архивировать проект [#861 #240714] 0 6

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -4,37 +4,23 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public static void packFiles(List<File> sources, File target) {
+    public static void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(
                 new BufferedOutputStream(
                         new FileOutputStream(target)))) {
-            for (File file : sources) {
-                zip.putNextEntry(new ZipEntry(file.getPath()));
-                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            for (Path file : sources) {
+                zip.putNextEntry(new ZipEntry(file.toFile().getPath()));
+                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file.toFile()))) {
                     zip.write(in.readAllBytes());
-                } catch (IOException io) {
-                    io.printStackTrace();
                 }
             }
         } catch (IOException io) {
             io.printStackTrace();
-        }
-    }
-
-    public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -52,9 +38,6 @@ public class Zip {
         } catch (IOException e) {
         e.printStackTrace();
         }
-        List<File> files = paths.stream()
-                .map(p -> p.toFile())
-                .collect(Collectors.toList());
-        packFiles(files, new File(argsName.get("o")));
+        packFiles(paths, new File(argsName.get("o")));
     }
 }
