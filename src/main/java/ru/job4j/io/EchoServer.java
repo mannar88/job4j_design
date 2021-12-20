57e63751076486309f2facacc0acacb90b062446
Viktor <mannar@inbox.ru>
2021-12-20 09:46:25 +0500

    1. Бот [#7921] 0 5

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,6 +8,20 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static String search(String str, ServerSocket serverSocket) throws IOException {
+        String rsl = null;
+        if (str.contains("msg=Exit")) {
+            rsl = " Exit";
+            serverSocket.close();
+        } else if (str.contains("Hello")) {
+            rsl = "Hello";
+        } else {
+            rsl = "What";
+        }
+        return rsl;
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -19,15 +33,15 @@ public class EchoServer {
                     answer = "HTTP/1.1 200 OK\r\n\r\n";
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
-                            answer = "HTTP/1.1 200 close\r\n\r\n";
-                            server.close();
+                        if (str.contains("GET")) {
+                            answer = search(str, server);
                         }
                     }
+                    out.write("HTTP/1.1 200 ok\r\n\r\n".getBytes());
                     out.write(answer.getBytes());
                     out.flush();
                 }
-                            }
+            }
 
 
         }
