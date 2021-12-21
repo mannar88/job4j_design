5455f422e7aed6b1361863e28a1f354bfadbcdb2
Viktor <mannar@inbox.ru>
2021-12-21 10:16:24 +0500

    2. Формат JSON [#313164] 0 5

--- src/main/java/ru/job4j/serialization/JsonSer.java
+++ src/main/java/ru/job4j/serialization/JsonSer.java
@@ -2,17 +2,16 @@ package ru.job4j.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class JsonSer {
     private final boolean bool = true;
-    private int anInt = 1;
-    private String string = "test";
-    private char[] chars = {'a', 'b', 'c'};
-    private List<Double> list = List.of(5.0);
+    private final int anInt = 1;
+    private final String string = "test";
+    private final char[] chars = {'a', 'b', 'c'};
+    private final List<Double> list = List.of(5.0);
 
     public static void main(String[] args) {
         JsonSer jsonSer = new JsonSer();
@@ -25,12 +24,12 @@ public class JsonSer {
     @Override
     public boolean equals(Object o) {
         if (this == o) {
-return true;
+            return true;
         }
-if (o == null || getClass() != o.getClass()) {
-return false;
-}
-JsonSer jsonSer = (JsonSer) o;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        JsonSer jsonSer = (JsonSer) o;
         return bool == jsonSer.bool && anInt == jsonSer.anInt && Objects.equals(string, jsonSer.string) && Arrays.equals(chars, jsonSer.chars) && Objects.equals(list, jsonSer.list);
     }
 
