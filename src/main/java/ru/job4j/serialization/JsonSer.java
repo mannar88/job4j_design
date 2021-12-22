84e8c97291d77cd9444074dfce76fd45e9cdaa32
Viktor <mannar@inbox.ru>
2021-12-22 05:20:11 +0500

    5. Преобразование JSON в POJO. JsonObject [#315064] 1 3

--- src/main/java/ru/job4j/serialization/JsonSer.java
+++ src/main/java/ru/job4j/serialization/JsonSer.java
@@ -1,25 +1,16 @@
 package ru.job4j.serialization;
 
-import javax.xml.bind.JAXBContext;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
+
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 
-@XmlRootElement(name = "jsonSer")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class JsonSer {
-    @XmlAttribute
     private boolean bool;
-    @XmlAttribute
     private int anInt;
     private String string;
     private char[] chars;
@@ -39,21 +30,9 @@ public class JsonSer {
 
     public static void main(String[] args) throws JAXBException, IOException {
         JsonSer jsonSer = new JsonSer(true, 1, "test", new char[]{'a', 'b', 'c'}, List.of(5.0));
-        JAXBContext context = JAXBContext.newInstance(JsonSer.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
-        try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(jsonSer, writer);
-            xml = writer.getBuffer().toString();
-            System.out.println(xml);
-        }
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            JsonSer result = (JsonSer) unmarshaller.unmarshal(reader);
-            System.out.println(result);
-        }
-
+        Gson gsonBuilder = new GsonBuilder().create();
+        JSONObject jsonObject = new JSONObject(gsonBuilder.toJson(jsonSer));
+        System.out.println(jsonObject);
     }
 
     @Override
