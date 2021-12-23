652e86ac17c0fb3e6d1a465393fc0655035d512b
Viktor <mannar@inbox.ru>
2021-12-23 11:57:53 +0500

    0. Что такое база данных. [#6860] 0 5

---
+++ script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE
+CREATE TABLE
+INSERT 0 1
+INSERT 0 1
+ id | name | age | bool 
+----+------+-----+------
+  1 | Tom  |  33 | f
+  2 | Tom  |  33 | f
+(2 строки)
+
+ id | name | age | bool 
+----+------+-----+------
+  1 | Tom  |  33 | f
+  2 | Tom  |  33 | f
+(2 строки)
+
+DELETE 2
