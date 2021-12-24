de0d477791b70385be47d0bc186e6bfda16f267f
Viktor <mannar@inbox.ru>
2021-12-24 06:24:40 +0500

    0. Что такое база данных. [#6860] 0 5

--- script.sql
+++ script.sql
@@ -1,17 +1,6 @@
-CREATE DATABASE
-CREATE TABLE
-INSERT 0 1
-INSERT 0 1
- id | name | age | bool 
-----+------+-----+------
-  1 | Tom  |  33 | f
-  2 | Tom  |  33 | f
-(2 строки)
-
- id | name | age | bool 
-----+------+-----+------
-  1 | Tom  |  33 | f
-  2 | Tom  |  33 | f
-(2 строки)
-
-DELETE 2
+create database test;
+\conect test;
+create table users (Id serial primary key, Name character varying(30), Age integer,  Bool boolean);
+insert into users (Name, Age, Bool) values ('Tom', 33, false);
+select * from users;
+delete from  users;
