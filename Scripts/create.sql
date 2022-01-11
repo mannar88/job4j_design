abee864762e1e4181cba7bc77bf7a038594502b4
Viktor <mannar@inbox.ru>
2022-01-11 07:29:57 +0500

    1. Схема прав пользователи и роли. [#1093 #249069] 0 6                   1. Схема прав пользователи и роли. [#1093] 0 6

--- Scripts/create.sql
+++ Scripts/create.sql
@@ -1,10 +1,9 @@
 create table users ( id serial primary key,
-name text
-);
+name text,
+role_id int references role(id));
 
 create table role( id serial primary key,
-name text,
-users_id int references users(id));
+name text);
 
 create table roles (id serial primary key,
  name text);
@@ -15,16 +14,20 @@ role_id int references role (id));
 
 create table item (id serial primary key,
 name text,
-users_id int references users(id));
+users_id int references users(id),
+category_id int references category (id),
+state_id int references state(id));
+
+create table comments ( id serial primary key,
+comment text,
+item_id int rferences item(id));
 
 create table attachs ( id serial primary key,
 name text,
 item_id int references item (id));
 
 create table category  ( id serial primary key,
-name text,
-item_id int references item (id));
+name text);
 
 create table state  ( id serial primary key,
-name text,
-item_id int references item (id));
+name text);
--- Scripts/insert.sql
+++ Scripts/insert.sql
@@ -1,17 +1,18 @@
-insert into users(name) values ('test');
+insert into role (name) values('test_role');
 
-insert into role (name, users_id) values('test_role', 1);
+insert into users(name, role_id) values ('test_user', 1);
 
 insert into roles(name) values('test_roles');
 
 insert into role_roles(role_id, roles_id) values (1, 1);
 
-insert into item(name, users_id) values ('test_item', 1);
+insert into category(name) values('test_category');
+
+insert into state(name) values('test_state');
+
+insert into item(name, users_id, category_id, state_id) values ('test_item', 1, 1, 1);
 
 insert into comments(comment, item_id) values('test_comment', 1);
 
 insert into attachs(name, item_id) values('test_attachs', 1);
 
-insert into category(name, item_id) values('test_category', 1);
-
-insert into state(name, item_id) values('test_state', 1);
