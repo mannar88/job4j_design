79ab5970d257c83174b1af32468c6afcc5f85ed0
Viktor <mannar@inbox.ru>
2022-01-24 19:36:06 +0500

    0. JDBC [#6863] 0 6

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
         Config config = new Config("./src/main/resources/app.properties");
         config.load();
+        Class.forName(config.value("driver"));
         try (Connection connection = DriverManager.getConnection(config.value("url"), config.value("login"), config.value("password"))) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
