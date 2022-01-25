2e002c9a2cbf7c00d2f4aa4bb06d986e357e64f0
Viktor <mannar@inbox.ru>
2022-01-25 18:51:11 +0500

    0.2. PrepareStatement [#379307] 0 9

---
+++ src/main/java/ru/job4j/jdbc/ImportDB.java
@@ -0,0 +1,68 @@
+package ru.job4j.jdbc;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class ImportDB {
+
+    private final Properties cfg;
+    private final String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./dump.txt");
+        db.save(db.load());
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            users = rd.lines()
+                    .map(s -> new User(s.substring(0, s.indexOf(";")), s.substring(s.indexOf(";") + 1, s.length() - 1)))
+                    .collect(Collectors.toList());
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users (name, email) values (?,?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+}
